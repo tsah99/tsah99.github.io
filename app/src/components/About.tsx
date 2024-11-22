32db2900f5c2da1fbb1b01202f688af17340b40c
Timothy Sah <timothysah@gmail.com>
2024-11-22 13:03:54 -0800

    fix

---
+++ app/src/components/About.tsx
@@ -0,0 +1,9 @@
+function About() {
+    return <div>
+        <h2>About</h2>
+        <p>about goes here</p>
+        </div>;
+  }
+  
+  export default About;
+  
---
+++ app/src/components/Experience.tsx
@@ -0,0 +1,9 @@
+function Experience() {
+    return <div>
+        <p>Experience</p>
+        <p>experience goes here</p>
+        </div>;
+  }
+  
+  export default Experience;
+  
